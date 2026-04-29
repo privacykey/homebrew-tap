bb95160f87cef4f579e6b3ccdb26bdfd6ac7b34f
AdamXweb <6800453+AdamXweb@users.noreply.github.com>
2026-04-29 15:32:31 +0000

    privacycommand 0.1.4

--- Casks/privacycommand.rb
+++ Casks/privacycommand.rb
@@ -2,8 +2,8 @@ cask "privacycommand" do
   # Bump on every release. The CI workflow prints the new sha256
   # alongside the DMG attachment URL — paste both in here, commit
   # against main, push.
-  version "0.1.3"
-  sha256 "a134fcf1baedcf21c2874843d6c93e43e8e56e48af76ca535658e7ae54deb889"
+  version "0.1.4"
+  sha256 "8fa90a421cad608307e21aa549eb28aec2be4775e33b18c82d141561f22feb05"
 
   url "https://github.com/privacykey/privacycommand/releases/download/v#{version}/privacycommand-#{version}.dmg"
   name "privacycommand"
